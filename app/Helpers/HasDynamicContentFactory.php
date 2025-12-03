3b6c7a3e018058b9c8eb7c665b848643ed141d9a
Swolley <myentity@inwind.it>
2025-12-03 23:13:11 +0100

    refactor: Simplify loading of presettable in HasDynamicContentFactory and remove commented-out code for improved clarity and maintainability

--- app/Helpers/HasDynamicContentFactory.php
+++ app/Helpers/HasDynamicContentFactory.php
@@ -12,7 +12,6 @@ use Modules\Cms\Casts\EntityType;
 use Modules\Cms\Casts\FieldType;
 use Modules\Cms\Models\Entity;
 use Modules\Cms\Models\Field;
-use Modules\Cms\Models\Pivot\Presettable;
 use RuntimeException;
 use stdClass;
 
@@ -78,31 +77,14 @@ trait HasDynamicContentFactory
     {
         throw_unless($model->entity_id, RuntimeException::class, 'No entity specified for model: ' . $model::class);
 
-        $model->loadRelation('presettable');
+        $model->load('presettable');
 
         /** @var class-string<Model&HasDynamicContents> $model_name */
         $model_name = $this->modelName();
 
         throw_unless(isset($this->entityType), RuntimeException::class, 'Entity type not set for model: ' . $model_name);
 
-        // /** @var Collection<Presettable> $all_presettables */
-        // $all_presettables = $model_name::fetchAvailablePresettables($this->entityType);
-
-        // $presettable = $model->presettable_id
-        //     ? $all_presettables->where('entity_id', $model->entity_id)->where('id', $model->presettable_id)->first()
-        //     : $all_presettables->where('entity_id', $model->entity_id)->random();
-
-        // if (! $presettable) {
-        //     /** @var HasDynamicContents $model */
-        //     throw new RuntimeException("No presettable found for entity: {$model->entity->name}");
-        // }
-
-        // /** @var HasDynamicContents $model */
-        // $model->presettable_id = $presettable->id;
-        // $model->entity_id = $presettable->entity_id;
-
-        // set the components depending on the preset configured fields
-        $model->components = $presettable->preset->fields->mapWithKeys(function (Field $field) use ($forcedValues): array {
+        $model->components = $model->presettable->preset->fields->mapWithKeys(function (Field $field) use ($forcedValues): array {
             $value = $field->pivot->default;
 
             if ($field->pivot->is_required || fake()->boolean()) {
