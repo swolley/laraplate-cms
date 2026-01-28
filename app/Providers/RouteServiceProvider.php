d8841066a614bd89077eda9509cf7d95ba8e05a5
Swolley <myentity@inwind.it>
2026-01-29 00:21:31 +0100

    refactor: Simplify RouteServiceProvider by removing redundant route mapping methods and updating name property visibility

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,57 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Providers;
 
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
+use Modules\Core\Overrides\RouteServiceProvider as ServiceProvider;
 
 final class RouteServiceProvider extends ServiceProvider
 {
-    private string $name = 'Cms';
-
-    /**
-     * Define the routes for the application.
-     */
-    public function map(): void
-    {
-        $this->mapApiRoutes();
-        $this->mapWebRoutes();
-    }
-
-    private function getPrefix(): string
-    {
-        return Str::slug($this->name);
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     */
-    private function mapWebRoutes(): void
-    {
-        Route::middleware('web')
-            ->prefix('app')
-            ->name($this->getPrefix() . '.')
-            ->group([
-                module_path($this->name, '/routes/web.php'),
-            ]);
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     */
-    private function mapApiRoutes(): void
-    {
-        $name_prefix = $this->getPrefix();
-        $route_prefix = 'api';
-        Route::prefix($route_prefix . '/v1')
-            ->middleware($route_prefix)
-            ->name(sprintf('%s.%s.', $name_prefix, $route_prefix))
-            ->group([
-                module_path($this->name, '/routes/api.php'),
-            ]);
-    }
+    protected string $name = 'Cms';
 }
